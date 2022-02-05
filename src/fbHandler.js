59631309c7d2a5534e0f68b19542a125643fc8e1
buitongminhchau01032002 <buitongminhchau01032002@gmail.com>
2022-02-05 14:10:23 +0700

    dj

--- src/fbHandler.js
+++ src/fbHandler.js
@@ -5,43 +5,25 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
-    let response;
-
     // Checks if the message contains text
-    if (received_message.text) {
+    if (received_message.quick_reply) {
         // Create the payload for a basic text message, which
         // will be added to the body of our request to the Send API
-        response = {
-            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-        }
-    } else if (received_message.attachments) {
-        // Get the URL of the message attachment
-        let attachment_url = received_message.attachments[0].payload.url;
-        response = {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [{
-                        "title": "Is this the right picture?",
-                        "subtitle": "Tap a button to answer.",
-                        "image_url": attachment_url,
-                        "buttons": [
-                            {
-                                "type": "postback",
-                                "title": "Yes!",
-                                "payload": "yes",
-                            },
-                            {
-                                "type": "postback",
-                                "title": "No!",
-                                "payload": "no",
-                            }
-                        ],
-                    }]
-                }
-            }
-        }
+        handleQuickReply(sender_psid, received_message);
+    } 
+}
+
+const handleQuickReply = (sender_psid, received_message) => {
+    let payload = received_message.quick_reply.payload;
+    switch (payload) {
+        case 'START':
+            sendMess.replySTART(sender_psid);
+            break;
+        case 'COIN_START':
+            sendMess.replyCOIN_START(sender_psid);
+            break;
+        default:
+            console.log('Incorrect quick reply');
     }
 }
 
