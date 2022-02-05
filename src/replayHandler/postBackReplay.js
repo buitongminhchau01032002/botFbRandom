a4b4c9dbb55b1ac65d7ba40c1a1451a5bdcbc58b
buitongminhchau01032002 <buitongminhchau01032002@gmail.com>
2022-02-05 12:22:32 +0700

    u

--- src/replayHandler/postBackReplay.js
+++ src/replayHandler/postBackReplay.js
@@ -4,40 +4,47 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-    // Construct the message body
-    let request_body = {
-        "recipient": {
-            "id": sender_psid
-        },
-        "message": response
-    }
-
-    // Send the HTTP request to the Messenger Platform
-    request({
-        "uri": "https://graph.facebook.com/v12.0/me/messages",
-        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-        "method": "POST",
-        "json": request_body
-    }, (err, res, body) => {
-        if (!err) {
-            console.log('message sent!')
-        } else {
-            console.error("Unable to send message:" + err);
+    return new Promise((resolve, reject) => {
+        // Construct the message body
+        let request_body = {
+            "recipient": {
+                "id": sender_psid
+            },
+            "message": response
         }
-    });
+
+        // Send the HTTP request to the Messenger Platform
+        request({
+            "uri": "https://graph.facebook.com/v12.0/me/messages",
+            "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+            "method": "POST",
+            "json": request_body
+        }, (err, res, body) => {
+            if (!err) {
+                console.log('message sent!');
+                return resolve('');
+            } else {
+                console.error("Unable to send message:" + err);
+                return reject(err);
+            }
+        });
+    })
+
 }
 
 const replayGET_STARTED = async (sender_psid) => {
     let response1;
     let profile = await getProfile(sender_psid);
-    response1 = { "text": `Xin chào ${profile.first_name} ${profile.last_name}, mình là Mitoo` };
-    callSendAPI(sender_psid, response1);
+    response1 = { "text": `Xin chào ${profile.last_name} ${profile.first_name}, mình là Mitoo` };
+    let response2 = getMainMenu();
+    await callSendAPI(sender_psid, response1);
+    await callSendAPI(sender_psid, response2);
 }
 
 
 
 const getProfile = (sender_psid) => {
-    return new Promise((resolve,reject) => {
+    return new Promise((resolve, reject) => {
         request({
             "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name&access_token=${PAGE_ACCESS_TOKEN}`,
             "method": "GET",
@@ -55,6 +62,39 @@ const getProfile = (sender_psid) => {
     })
 }
 
+
+const getMainMenu = () => {
+    return {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "Mình là Mitoo",
+                    "subtitle": "Bạn có thể trò chuyện với Mitoo bằng một số lựa chọn bên dưới",
+                    "image_url":"https://picsum.photos/id/102/300/400",
+                    "buttons": [
+                        {
+                            "type": "postback",
+                            "title": "Mitoo là ai?",
+                            "payload": "ABOUT",
+                        },
+                        {
+                            "type": "postback",
+                            "title": "Bắt đầu ngay",
+                            "payload": "START",
+                        },
+                        {
+                            "type": "postback",
+                            "title": "Góp ý cho Mitoo",
+                            "payload": "FEED_BACK",
+                        }
+                    ],
+                }]
+            }
+        }
+    }
+}
 
 module.exports = {
     replayGET_STARTED
