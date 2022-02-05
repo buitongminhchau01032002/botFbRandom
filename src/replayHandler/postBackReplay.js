8ca508d01ec4ce0245a4b055d536c850de39dee6
buitongminhchau01032002 <buitongminhchau01032002@gmail.com>
2022-02-05 11:15:47 +0700

    ut

--- src/replayHandler/postBackReplay.js
+++ src/replayHandler/postBackReplay.js
@@ -1,9 +1,32 @@
-const fbHandler = require('../fbHandler');
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v12.0/me/messages",
+        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
 
 const replayGET_STARTED = (sender_psid) => {
     let response1;
     response1 = { "text": 'Xin chào ABC, mình là Mitoo' };
-    fbHandler.callSendAPI(sender_psid, response1);
+    callSendAPI(sender_psid, response1);
 }
 
 module.exports = {
