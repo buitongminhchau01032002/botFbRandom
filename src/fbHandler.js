8d04320d07e2d8c2472f3b78b582a3b5b8729eb2
buitongminhchau01032002 <buitongminhchau01032002@gmail.com>
2022-02-05 20:30:27 +0700

    F

--- src/fbHandler.js
+++ src/fbHandler.js
@@ -4,7 +4,7 @@ const sendMess = require('./replyHandler/postBackReply');
 function handleMessage(sender_psid, received_message) {
     if (received_message.quick_reply) {
         handleQuickReply(sender_psid, received_message);
-    } 
+    }
 }
 
 const handleQuickReply = (sender_psid, received_message) => {
@@ -36,6 +36,12 @@ function handlePostback(sender_psid, received_postback) {
             break;
         case 'COIN_START':
             sendMess.replyCOIN_START(sender_psid);
+            break;
+        case 'DICE_START':
+            sendMess.replyDICE_START(sender_psid);
+            break;
+        case 'DICE_1':
+            sendMess.replyDICE_FINISH(sender_psid, 1);
             break;
         default:
             console.log('Incorrect post back');
