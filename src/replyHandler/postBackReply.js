0e0e1b55dd59bc8fd9db3e547beebe5b67ea83c0
buitongminhchau01032002 <buitongminhchau01032002@gmail.com>
2022-02-06 18:11:44 +0700

    F

--- src/replyHandler/postBackReply.js
+++ src/replyHandler/postBackReply.js
@@ -196,10 +196,10 @@ const replyUnknown = async (sender_psid) => {
         text: `Ohh, ${BOT} không hiểu tin nhắn của bạn 😭😭`
     });
     await callSendAPI(sender_psid, {
-        text: `­\n     🔴      \n `
+        text: `­\n     🔴      \n­ `
     });
     await callSendAPI(sender_psid, {
-        text: `­\n🔴      🔴\n   `
+        text: `­\n🔴      🔴\n ­  `
     });
     await callSendAPI(sender_psid, {
         text: `­🔴\n     🔴\n           🔴`
